3065615d2ce5207c54fdcd61de056bb7a48bce36
jaraco@jaraco.com <jaraco@jaraco.com>
2008-10-05 19:03:52 +0000

    Added a setuptools installer script, which supercedes the autocode script.  This package can now be uploaded to PyPI (Python Package Index). The setup script includes a console entry point.  After running setup.py develop or setup.py install, a script called sqlautocode will be created in the platform's scripts directory.

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!python
+# -*- coding: UTF-8 -*-
+
+"""
+Setup script for building sqlautocode
+"""
+
+version = open('version.txt').next().strip()
+
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup (name = 'sqlautocode',
+		version = version,
+		description = 'AutoCode is a flexible tool to autogenerate a model from an existing database.',
+		author = 'Simon Pamies',
+		author_email = 's.pamies@banality.de',
+		url = 'http://code.google.com/p/sqlautocode/',
+		packages = find_packages(exclude=['ez_setup', 'tests']),
+		zip_safe=True,
+		license = 'MIT',
+		classifiers = [
+			"Development Status :: 4 - Beta",
+			"Intended Audience :: Developers",
+			"Programming Language :: Python",
+		],
+		entry_points = dict(
+			console_scripts = [
+				'sqlautocode = sqlautocode:main',
+			],
+		),
+		install_requires=[
+		],
+		extras_require = {
+		},
+		dependency_links = [
+		],
+		tests_require=[
+			'nose>=0.10',
+		],
+		test_suite = "nose.collector",
+	)
