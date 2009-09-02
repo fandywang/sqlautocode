f1f65b1151c3a0b687a96c6223acec3fb50f684b
percious <devnull@localhost>
2009-09-01 21:48:43 -0600

    added the ability to automatically generate declarative classes from table schema.  Oh, the insanity!

--- sqlautocode/declarative.py
+++ sqlautocode/declarative.py
@@ -1,21 +1,129 @@
-import sys
+import sys, re
 from util import emit
 from sqlalchemy.ext.sqlsoup import SqlSoup
+from sqlalchemy import MetaData
+from sqlalchemy.ext.declarative import declarative_base, _deferred_relation
+from sqlalchemy.orm import relation, backref
 
+# lifted from http://www.daniweb.com/forums/thread70647.html
+# (pattern, search, replace) regex english plural rules tuple
+rule_tuple = (
+('[ml]ouse$', '([ml])ouse$', '\\1ice'), 
+('child$', 'child$', 'children'), 
+('booth$', 'booth$', 'booths'), 
+('foot$', 'foot$', 'feet'), 
+('ooth$', 'ooth$', 'eeth'), 
+('l[eo]af$', 'l([eo])af$', 'l\\1aves'), 
+('sis$', 'sis$', 'ses'), 
+('man$', 'man$', 'men'), 
+('ife$', 'ife$', 'ives'), 
+('eau$', 'eau$', 'eaux'), 
+('lf$', 'lf$', 'lves'), 
+('[sxz]$', '$', 'es'), 
+('[^aeioudgkprt]h$', '$', 'es'), 
+('(qu|[^aeiou])y$', 'y$', 'ies'), 
+('$', '$', 's')
+)
+ 
+def regex_rules(rules=rule_tuple):
+    for line in rules:
+        pattern, search, replace = line
+        yield lambda word: re.search(pattern, word) and re.sub(search, replace, word)
+ 
+def plural(noun):
+    for rule in regex_rules():
+        result = rule(noun)
+        if result: 
+            return result
 
+def name2label(name):
+    """
+    Convert a column name to a Human Readable name.
+    borrowed from old TG fastdata code
+    """
+    # Create label from the name:
+    #   1) Convert _ to spaces
+    #   2) Convert CamelCase to Camel Case
+    #   3) Upcase first character of Each Word
+    # Note: I *think* it would be thread-safe to
+    #       memoize this thing.
+    return str(' '.join([s.capitalize() for s in
+               re.findall(r'([A-Z][a-z0-9]+|[a-z0-9]+|[A-Z0-9]+)', name)])).replace(' ', '')
 
 class ModelFactory(object):
     
     def __init__(self, config):
         self.config = config
-        
-    @property
-    def soup(self):
-        if hasattr(self, '_soup'):
-            return self._soup
-        s = SqlSoup(self.config.engine)
         schema = getattr(self.config, 'schema', None)
+        self._metadata = MetaData(bind=config.engine)
         if schema:
-            s.schama = schema
-        self._soup = s
-        return s
+            self._metadata.schema = schema
+        self._metadata.reflect()
+        
+        self.DeclarativeBase = declarative_base(metadata=self._metadata)
+
+    @property
+    def tables(self):
+        return self._metadata.tables.keys()
+    
+    def create_model(self, table):
+        #partially borred from Jorge Vargas' code
+        #http://dpaste.org/V6YS/
+        
+        model_name = name2label(table.name)
+        class Temporal(self.DeclarativeBase):
+            __table__ = table
+
+        #hack the class to have the right classname
+        Temporal.__name__ = model_name
+        
+        #trick sa's model registry to think the model is the correct name
+        Temporal._decl_class_registry[model_name] = Temporal._decl_class_registry['Temporal']
+        del Temporal._decl_class_registry['Temporal']
+
+        #add in single relations
+        for column in self.get_foreign_keys(table):
+            related_table = column.foreign_keys[0].column.table
+            backref = plural(table.name)
+            setattr(Temporal, related_table.name, _deferred_relation(Temporal, relation(name2label(related_table.name), backref=backref)))
+        
+        #add in many-to-many relations
+        for join_table in self.get_related_many_to_many_tables(table.name):
+            for column in join_table.columns:
+                key = column.foreign_keys[0]
+                if key.column.table is not table:
+                    related_table = column.foreign_keys[0].column.table
+                    backref = plural(table.name)
+                    setattr(Temporal, plural(related_table.name), _deferred_relation(Temporal, relation(name2label(related_table.name), secondary=join_table)))
+                    break;
+
+        return Temporal
+
+    def get_table(self, name):
+        """(name) -> sqlalchemy.schema.Table
+        get the table definition with the given table name
+        """
+        if hasattr(self._metadata, 'schema'):
+            schema = self._metadata.schema
+            if schema and not name.startswith(schema):
+                name = '.'.join((schema, name))
+        return self._metadata.tables[name]
+
+    def get_foreign_keys(self, table):
+        return [column for column in table.columns if len(column.foreign_keys)>0]
+
+    def get_many_to_many_tables(self):
+        if not hasattr(self, '_many_to_many_tables'):
+            self._many_to_many_tables = [table for table in self._metadata.tables.values() if len(self.get_foreign_keys(table)) == 2 and len(table.c) == 2]
+        return self._many_to_many_tables
+
+    def get_related_many_to_many_tables(self, table_name):
+        tables = []
+        src_table = self.get_table(table_name)
+        for table in self.get_many_to_many_tables():
+            for column in table.columns:
+                key = column.foreign_keys[0]
+                if key.column.table is src_table:
+                    tables.append(table)
+                    break
+        return tables
--- sqlautocode/tests/test_declarative.py
+++ sqlautocode/tests/test_declarative.py
@@ -1,7 +1,12 @@
+import os
+from nose.tools import eq_
 from sqlautocode.declarative import ModelFactory
+from sqlalchemy.orm import class_mapper
+testdb = os.path.abspath(os.path.dirname(__file__))+'/data/devdata.db'
+
+print testdb
 class DummyConfig:
-    engine = "postgres://postgres@localhost/TestUsers"
-    schema = 'pdil_db'
+    engine = 'sqlite:///'+testdb
 
 class TestModelFactory:
     
@@ -9,7 +14,32 @@ class TestModelFactory:
         self.config = DummyConfig()
         self.factory = ModelFactory(self.config)
     
-    def test_soup(self):
-        soup = self.factory.soup
-        soup.migrate_version
-        import ipdb; ipdb.set_trace()
+    def test_tables(self):
+        tables = sorted(self.factory.tables)
+        eq_(tables,  [u'tg_group', u'tg_group_permission', u'tg_permission', u'tg_town', u'tg_user', u'tg_user_group'])
+    
+    def test_create_model(self):
+        Group      = self.factory.create_model(self.factory._metadata.tables['tg_group'])
+        Permission = self.factory.create_model(self.factory._metadata.tables['tg_permission'])
+        Town       = self.factory.create_model(self.factory._metadata.tables['tg_town'])
+        User       = self.factory.create_model(self.factory._metadata.tables['tg_user'])
+
+        class_mapper(Town)
+        class_mapper(User)
+        
+        t = Town(town_name="Arvada")
+        u = User(tg_town=t)
+        assert u.tg_town.town_name == 'Arvada'
+        
+    def test_get_many_to_many_tables(self):
+        tables = sorted([table.name for table in self.factory.get_many_to_many_tables()])
+        eq_(tables, [u'tg_group_permission', u'tg_user_group'])
+    
+    def test_get_related_many_to_many_tables(self):
+        tables = [table.name for table in self.factory.get_related_many_to_many_tables('tg_user')]
+        eq_(tables, [u'tg_user_group'])
+        
+    def test_get_foreign_keys(self):
+        columns = [column.name for column in sorted(self.factory.get_foreign_keys(self.factory._metadata.tables['tg_user']))]
+        eq_(columns, ['town_id'])
+    
